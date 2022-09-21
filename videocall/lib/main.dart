9e4463a729fe912c2d692e8c08a716e4ae622630
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2022-09-21 16:03:05 -0300

    update main
    update main

--- videocall/lib/main.dart
+++ videocall/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_call/routes/pages.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(GetMaterialApp(
+    initialRoute: Pages.INITIAL,
+    getPages: Pages.routes,
+    debugShowCheckedModeBanner: false,
+  ));
 }
